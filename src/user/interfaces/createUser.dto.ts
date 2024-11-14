64ca8dbcada36374b1eba47cecf2ca1076fb8075
VictorSantuccii <victorfregne@gmail.com>
2024-11-14 14:30:06 -0300

    feat(user-validation): refactoring user creation and update validations

--- src/user/interfaces/createUser.dto.ts
+++ src/user/interfaces/createUser.dto.ts
@@ -1,30 +1,33 @@
-import { IsString } from "class-validator";
+import { IsString, IsEmail, IsEnum, IsPhoneNumber, IsOptional, Length, Matches } from 'class-validator';
 
 export enum UserRole {
-    Admin = 'admin',
-    User = 'user',
+  Admin = 'admin',
+  User = 'user',
 }
 
 export class CreateUserDto {
 
-    @IsString()
-    name: string;
+  @IsString()
+  @Length(3, 50, { message: 'O nome deve ter entre 3 e 50 caracteres' })
+  name: string;
 
-    @IsString()
-    email: string;
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email: string;
 
+  @IsPhoneNumber('BR', { message: 'Número de telefone inválido' })
+  phone: string;
 
-    @IsString()
-    phone: string;
+  @IsString()
+  @Length(11, 11, { message: 'O CPF deve ter exatamente 11 dígitos' })
+  @Matches(/^\d+$/, { message: 'O CPF deve conter apenas números' })
+  cpf: string;
 
+  @IsString()
+  @Length(8, 20, { message: 'A senha deve ter entre 8 e 20 caracteres' })
+  @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/, { message: 'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial' })
+  password: string;
 
-    @IsString()
-    cpf: string;
-
-
-    @IsString()
-    password: string;
-
-
-
+  @IsOptional()
+  @IsEnum(UserRole, { message: 'O papel do usuário deve ser "admin" ou "user"' })
+  role?: UserRole;
 }
--- src/user/interfaces/updateUser.dto.ts
+++ src/user/interfaces/updateUser.dto.ts
@@ -1,24 +1,29 @@
-import { IsOptional, IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsOptional, IsString, IsEmail, IsNotEmpty, Length, Matches, IsPhoneNumber } from 'class-validator';
 
 export class UpdateUserDto {
     @IsOptional()
     @IsString()
     @IsNotEmpty()
+    @Length(3, 50, { message: 'O nome deve ter entre 3 e 50 caracteres' })
     name?: string;
 
     @IsOptional()
-    @IsEmail()
+    @IsEmail({}, { message: 'E-mail inválido' })
     email?: string;
 
     @IsOptional()
     @IsString()
+    @Length(11, 11, { message: 'O CPF deve ter exatamente 11 dígitos' })
+    @Matches(/^\d+$/, { message: 'O CPF deve conter apenas números' })
     cpf?: string;
 
     @IsOptional()
-    @IsString()
+    @IsPhoneNumber('BR', { message: 'Número de telefone inválido' })
     phone?: string;
 
     @IsOptional()
     @IsString()
+    @Length(8, 20, { message: 'A senha deve ter entre 8 e 20 caracteres' })
+    @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/, { message: 'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial' })
     password?: string;
 }
